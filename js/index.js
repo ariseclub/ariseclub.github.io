878d6345fc683ea8208ffa30d14b3edd3c0e3b9c
Raúl <160028661+RaulClavo@users.noreply.github.com>
2025-10-16 02:20:34 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// Animaciones principales de la pagina web
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const elementos = document.querySelectorAll(".fade-in");
+    const observador = new IntersectionObserver((entries)=>{
+        entries.forEach(entry =>{
+            if(entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
+    }, { threshold: 0.2 })
+    elementos.forEach(el => observador.observe(el));
+}); 
+
+// Mostrar el header cuando no estemos en la primera vista
+
+const header = document.querySelector("header");
+const welcome = document.getElementById("welcome");
+const observador = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            header.classList.remove("visible");
+        } else {
+            header.classList.add("visible");
+        }
+    })
+})
+
+observador.observe(welcome);
